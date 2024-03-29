225c1de73a31a8492d2048c90546a78c4af11768
toniianugrah <toniianugrah@gmail.com>
2024-03-30 01:33:01 +0700

    update

--- layouts/default-layout.tsx
+++ layouts/default-layout.tsx
@@ -6,7 +6,7 @@ const DefaultLayout = ({ children }: Props) => {
   return (
     <div>
       <div className="bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div className="bg-pink-400 h-2 relative" />
+        <div className="bg-pink-300 h-1 relative" />
         <div className="max-w-xl pt-12 pb-16 mx-auto px-4 m-2">{children}</div>
       </div>
     </div>
